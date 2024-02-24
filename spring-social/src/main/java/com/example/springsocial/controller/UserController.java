e3383a980b913a1a4c90d4bdb6ef3b4e7f897b6b
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-02-24 15:17:57 +0700

    Create the custom event send to application insights when APIs is called from User Controller
    + Trigger /user/me

--- spring-social/src/main/java/com/example/springsocial/controller/UserController.java
+++ spring-social/src/main/java/com/example/springsocial/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.springsocial.model.User;
 import com.example.springsocial.repository.UserRepository;
 import com.example.springsocial.security.CurrentUser;
 import com.example.springsocial.security.UserPrincipal;
+import com.microsoft.applicationinsights.TelemetryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,14 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    private final TelemetryClient telemetryClient = new TelemetryClient();
+
+
     @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        telemetryClient.trackEvent("URI /user/me is triggered for user profile: " + userPrincipal.getEmail());
+
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
     }
