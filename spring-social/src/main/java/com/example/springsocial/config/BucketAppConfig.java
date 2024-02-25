27af1f2165d20c2f14d9cc75cb358c1ae582e2cb
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-02-25 15:38:34 +0700

    Create BucketAppConfig.java

---
+++ spring-social/src/main/java/com/example/springsocial/config/BucketAppConfig.java
@@ -0,0 +1,16 @@
+package com.example.springsocial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class BucketAppConfig implements WebMvcConfigurer {
+
+    private final RateLimitInterceptor interceptor;
+
+    public BucketAppConfig(RateLimitInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+    
+}
